1ef4031d43f7425a3f0fc62e62d607b7a0ef5cb4
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-10-14 13:33:59 +0500

    10 - Not Found Page

--- src/app/products/[productID]/reviews/[reviewID]/page.tsx
+++ src/app/products/[productID]/reviews/[reviewID]/page.tsx
@@ -1,8 +1,14 @@
+import { notFound } from "next/navigation";
+
+
 const ReviewList = ({
   params,
 }: {
   params: { productID: string; reviewID: string };
 }) => {
+  if (parseInt(params.reviewID) > 1000) {
+    return notFound();
+  }
   return (
     <div>
       <h1>Review {params.reviewID} for Product {params.productID}</h1>

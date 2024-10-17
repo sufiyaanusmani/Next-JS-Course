0c4321d442e5be50e7b2033e4f9b9a0e1f29c1ef
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-10-17 19:05:37 +0500

    17 - Routing Metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,9 @@
+export const metadata = {
+  title: "About",
+};
+
 const About = () => {
-    return <h1>About</h1>;
-  };
-  
-  export default About;
-  
+  return <h1>About</h1>;
+};
+
+export default About;
--- src/app/products/[productID]/page.tsx
+++ src/app/products/[productID]/page.tsx
@@ -1,3 +1,22 @@
+import { Metadata } from 'next';
+
+type Props = {
+  params: {
+    productID: string;
+  };
+}
+
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+  const title = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`Product Details ${params.productID}`);
+    }, 1000);
+  })
+  return {
+    title: title,
+  };
+}
+
 const ProductDetails = ({params} : {params: {productID: string}}) => {
   return (
     <div>
